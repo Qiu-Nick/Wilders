e287c2f2a83cde1425105149060cdc77d6226b39
Nck <nicolas.theaux.peria@gmail.com>
2024-10-15 11:20:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ import chalk from 'chalk';
 console.log(chalk.blue('Ludo'));
 console.log(chalk.red('Badr'));
 console.log(chalk.green('Simon'));
+console.log(chalk.grey('Clement'));
 console.log(chalk.yellow('Alex'));
